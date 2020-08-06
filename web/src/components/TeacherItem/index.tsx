af905654ad9570cb7b370cb032a89c3b161f1272
Juliano Rodrigo <julianormgalli@gmail.com>
2020-08-06 01:22:04 -0300

    Added TeacherForm (day 3)

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,32 +1,51 @@
 import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem(){
+export interface Teacher {
+    id: number
+    avatar: string
+    bio: string
+    cost: number
+    name: string
+    subject: string
+    whatsapp: string
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+
+    function createNewConnection(){
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
 
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/44454114?s=460&u=c641b2ff91972db792895d88fc5cccae6b16a820&v=4" alt="Juliano Rodrigo"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Juliano Rodrigo</strong>
-                    <span>Computação</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tortor leo, ultricies vel ante vitae, rhoncus cursus ligula. Vestibulum vel diam ac tellus interdum convallis. Cras sed dolor a erat eleifend ornare. Aliquam erat volutpat. Nunc vitae ullamcorper leo. Duis convallis nisl lacinia, cursus enim sed, commodo purus. Phasellus ligula lectus, placerat at lacinia ut, feugiat scelerisque neque. Cras fringilla, quam et dapibus ullamcorper, tellus massa consectetur nunc, ac iaculis ante odio sed magna. Cras eu quam libero. Nulla id dui lectus. Aenean quis sodales sem. Fusce elit lectus, luctus sit amet tellus quis, viverra congue leo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque id sem vel neque accumsan dignissim. 
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora 
-                    <strong>R$ 100,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Ícone do WhatsApp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
